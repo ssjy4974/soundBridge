7f67c82f980fab471ed6d6c53fd54ba14992d1e3
JunYongSun <ssjy4974@naver.com>
2023-03-09 14:47:21 +0900

    chore : 기본 config 설정

---
+++ backend/src/main/java/com/soundbridge/global/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.soundbridge.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+//
+//    private final CustomOAuth2UserService customOAuth2UserService;
+//    private final OAuth2SuccessHandler successHandler;
+//    private final TokenService tokenService;
+//    private final MemberRepository memberRepository;
+
+//    @Bean
+//    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.httpBasic().disable()      // Http basic Auth  기반으로 로그인 인증창이 뜸.  disable 시에 인증창 뜨지 않음.
+//            .csrf().disable()       // rest api이므로 csrf 보안이 필요없으므로 disable처리.
+//            .sessionManagement().sessionCreationPolicy(
+//                SessionCreationPolicy.STATELESS)// jwt token으로 인증하므로 stateless 하도록 처리.
+//            .and()// 인증권한이 필요한 페이지.// 나머지 모든 요청 허용  ( 생략 가능 )
+//                .oauth2Login()
+//                .successHandler(successHandler)
+//                .userInfoEndpoint().userService(customOAuth2UserService);
+////            .failureHandler(oAuth2AuthenticationFailureHandler);
+//
+//        http.addFilterAfter(new JwtAuthFilter(tokenService, memberRepository),
+//            UsernamePasswordAuthenticationFilter.class);
+//        return http.build();
+//    }
+
+    // 필터 무시
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/**","/static/**", "/js/**", "/webjars/**");
+    }
+
+}
