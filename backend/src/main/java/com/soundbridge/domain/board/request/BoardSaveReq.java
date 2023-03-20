261fbff1afb1ec376c5cc7e4add8c494e7e4e4d9
JunYongSun <ssjy4974@naver.com>
2023-03-20 14:05:22 +0900

    refactor : BoardSaveReq 수정

--- backend/src/main/java/com/soundbridge/domain/board/request/BoardSaveReq.java
+++ backend/src/main/java/com/soundbridge/domain/board/request/BoardSaveReq.java
@@ -10,6 +10,7 @@ import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Getter
 @Setter
@@ -19,18 +20,18 @@ public class BoardSaveReq {
     @NotEmpty(message = "title cannot be empty.")
     private String title;
     @NotNull(message = "startTime cannot be null")
-    @JsonFormat(shape = Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime startTime;
     @NotNull(message = "endTIme cannot be null")
-    @JsonFormat(shape = Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime endTime;
 
     public FeedbackBoard toEntity(Member member) {
         return FeedbackBoard.builder()
             .title(this.title)
             .member(member)
-            .startTime(this.startTime)
-            .endTime(this.endTime)
+            .startTime(this.startTime.plusHours(9))
+            .endTime(this.endTime.plusHours(9))
             .build();
     }
 }
