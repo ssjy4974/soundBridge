407e9f8474313438434c1dcbc187b4cbe944f2fb
오세솔 <tpthf1@gmail.com>
2023-03-21 17:14:28 +0900

    S08P22A703-354 feat : TTS 기능 구현

---
+++ ai/database.py
@@ -0,0 +1,26 @@
+from sqlalchemy import *
+from sqlalchemy.orm import sessionmaker
+import os
+import json
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+SERECT_FILE = os.path.join(BASE_DIR, 'secrets.json')
+serects = json.loads(open(SERECT_FILE).read())
+DB = serects["DB"]
+
+DB_URL = f"mysql+pymysql://{DB['user']}:{DB['password']}@{DB['host']}/{DB['database']}?charset=utf8"
+
+
+class engineconn:
+
+    def __init__(self):
+        self.engine = create_engine(DB_URL, pool_recycle=500)
+
+    def sessionmaker(self):
+        Session = sessionmaker(bind=self.engine)
+        session = Session()
+        return session
+
+    def connection(self):
+        conn = self.engine.connect()
+        return conn
