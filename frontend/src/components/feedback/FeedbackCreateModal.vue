02210f6c0e273d9dc8c145f9e4533505277b4a01
JunYongSun <ssjy4974@naver.com>
2023-03-21 17:20:16 +0900

    S08P22A703-260 feat : 피드백 상담 게시글 등록 API 연결

--- frontend/src/components/feedback/FeedbackCreateModal.vue
+++ frontend/src/components/feedback/FeedbackCreateModal.vue
@@ -1,23 +1,71 @@
 <template>
   <div class="base__modal">
     <p>제목</p>
-    <input type="text" placeholder="제목을 입력하세요" />
+    <input
+      v-model="feedbackArticleSaveReq.title"
+      type="text"
+      placeholder="제목을 입력하세요"
+    />
     <div>
-      <h3>임시용으로 만든 시간 모듈 참고해서 구현하기</h3>
-      <p>Start Date</p>
-      {{ startDate }}
-      <p>End Date</p>
-      {{ endDate }}
+      <VueDatePicker
+        v-model="startTime"
+        show-now-button
+        :min-date="new Date()"
+      ></VueDatePicker>
+      <VueDatePicker
+        v-model="endTime"
+        show-now-button
+        :min-date="new Date()"
+      ></VueDatePicker>
+      <button @click="feedbackBoardSave">전송</button>
     </div>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
+import { apiInstance } from "@/api/index";
+import VueDatePicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
 
-const startDate = ref(new Date());
-const endDate = ref(new Date());
-console.log(startDate.value);
+const api = apiInstance();
+const startTime = ref();
+const endTime = ref();
+
+const feedbackArticleSaveReq = ref({
+  title: undefined,
+  startTime: undefined,
+  endTime: undefined,
+});
+
+const feedbackBoardSave = () => {
+  if (!feedbackArticleSaveReq.value.title) {
+    alert("제목을 입력 해주세요");
+    return;
+  }
+
+  if (!startTime.value) {
+    alert("시작 시간를 선택 해주세요");
+    return;
+  }
+
+  if (!endTime.value) {
+    alert("종료 시간를 선택 해주세요");
+    return;
+  }
+
+  feedbackArticleSaveReq.value.startTime = startTime.value;
+  feedbackArticleSaveReq.value.endTime = endTime.value;
+  api
+    .post(`/api/feedback-boards`, feedbackArticleSaveReq.value)
+    .then(() => {
+      alert("등록 성공");
+    })
+    .catch((err) => {
+      err;
+      alert("등록 실패");
+    });
+};
 </script>
 
 <style scoped>
