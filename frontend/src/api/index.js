3336ecb4e969749d370855a896bfdc1fc5ca3813
JunYongSun <ssjy4974@naver.com>
2023-03-22 10:50:17 +0900

    refactor : index.js 주석

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 // local vue api axios instance
 function apiInstance() {
   const instance = axios.create({
-    baseURL: ${API_BASE_URL}, //env로 설정 해줘야함
+    // baseURL: ${API_BASE_URL}, //env로 설정 해줘야함
     headers: {
       "Content-Type": "application/json;charset=utf-8",
       accept: "application/json,",
