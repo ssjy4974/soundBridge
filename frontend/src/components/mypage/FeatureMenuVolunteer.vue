e77ccdd473a5af7b11c691ad6e7b04046921e5ea
오세솔 <tpthf1@gmail.com>
2023-03-22 17:23:14 +0900

    S08P22A703-311 feat : 기부 진행상황 확인

--- frontend/src/components/mypage/FeatureMenuVolunteer.vue
+++ frontend/src/components/mypage/FeatureMenuVolunteer.vue
@@ -4,15 +4,22 @@
       <hr />
       <p>TO Do : 봉사자용 마이페이지 메뉴 만드는중</p>
 
-      <span>발음 피드백 |</span>
-      <router-link to="/">
+      <router-link to="/mypagev/recordState">
         <span>목소리 기부 |</span>
       </router-link>
+
+      <router-link to="/mypagev/meetings">
+        <span>발음 피드백 |</span>
+      </router-link>
+
       <router-view></router-view>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import MyMeetings from "./MyMeetings.vue";
+import recordState from "./RecordState.vue";
+</script>
 
 <style scoped></style>
