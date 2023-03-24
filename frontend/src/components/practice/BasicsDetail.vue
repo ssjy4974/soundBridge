784d9ac0c7e84bed49a6b6d980cd91f241e8f070
JunYong Sun <ssjy4974@naver.com>
2023-03-24 14:46:01 +0900

    S08P22A703-282 S08P22A703-283 feat : 기본발음 시도횟수, 성공횟수 업데이트 API

--- frontend/src/components/practice/BasicsDetail.vue
+++ frontend/src/components/practice/BasicsDetail.vue
@@ -9,7 +9,10 @@
           alt="조음법 이미지"
         />
       </div>
-
+      <div>
+        <p>시도 횟수 : {{ basicLetter.tryCount }}</p>
+        <p>성공 횟수 : {{ basicLetter.successCount }}</p>
+      </div>
       <div>
         <div>
           <button @click="router.replace(`/practicebasics`)">
@@ -18,18 +21,16 @@
         </div>
         <div>
           <p>실제 발음 명 : {{ basicLetter.guidLetter }}</p>
-          <p>
-            유저가 연습한 발음 보여주기 (stt결과) 올바른 발음과 일치하면 성공횟
-            수 +1
-          </p>
+          <p>나의 발음</p>
+          <div class="transcript" v-text="transcript"></div>
         </div>
-        <!-- 자음 모음 다 추가하면 index는 수정할 예정 -->
+        <!-- 자음 모음 다 추가하면 index는 수정할 예정  -->
         <div v-if="route.params.basicLetterId < 14">
           <button @click="next">다음 버튼 (아이콘)</button>
         </div>
       </div>
       <div>
-        <button>연습하기 누르면 STT 실행</button>
+        <button @click="ToggleMic">연습</button>
       </div>
     </div>
   </div>
@@ -38,7 +39,7 @@
 <script setup>
 import { useRoute } from "vue-router";
 import { apiInstance } from "@/api/index";
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import router from "@/router/index";
 
 const api = apiInstance();
@@ -46,6 +47,80 @@ const route = useRoute();
 
 const basicLetter = ref();
 const IMAGE_PATH = import.meta.env.VITE_IMAGE_PATH;
+
+const transcript = ref("");
+const isRecording = ref(false);
+
+const Recognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const sr = new Recognition();
+
+onMounted(() => {
+  // 각각의 인식 결과마다 continuous한 results를 받을지 혹은 single result를 받을지 결정
+  // 디폴트 값은 false, single word를 리턴함
+  sr.continuous = false;
+  // 중간 결과를 리턴할지 아닐지 결정
+  // 아직 인식이 완료 되지 않은 중간 results
+  // 인식이 모두 완료되면 isFinal이 true, 아직 인식 전이면 false
+  // 인식이 모두 완료 되면 내가 발음 한 발음과 실제 발음을 비교하는 api 호출 함
+  sr.interimResults = true;
+  // 언어 설정, 설정하지 않으면 디폴트로 HTML 의 lang 속성값을 받음
+  sr.lang = "ko-KR";
+  sr.onstart = () => {
+    console.log("연습 시작");
+    isRecording.value = true;
+  };
+  sr.onend = () => {
+    console.log("연습 종료");
+    isRecording.value = false;
+  };
+  sr.onresult = (evt) => {
+    for (let i = 0; i < evt.results.length; i++) {
+      const result = evt.results[i];
+      if (result.isFinal) {
+        CheckSuccess(result);
+      }
+    }
+    const t = Array.from(evt.results)
+      .map((result) => result[0])
+      .map((result) => result.transcript)
+      .join("");
+    transcript.value = t;
+  };
+});
+
+const CheckSuccess = (result) => {
+  const t = result[0].transcript;
+  if (t == basicLetter.value.guidLetter) {
+    sr.stop();
+    api
+      .put(
+        `/api/try-histories/basic-letters/${basicLetter.value.basicLetterId}`
+      )
+      .then(() => {
+        basicLetter.value.successCount++;
+        alert("정답입니다.");
+      })
+      .catch((err) => {
+        alert("다시 한번 시도 해주세요");
+      });
+  } else {
+    sr.stop();
+    alert("실패");
+  }
+};
+
+const ToggleMic = () => {
+  api
+    .post(`/api/try-histories/basic-letters/${basicLetter.value.basicLetterId}`)
+    .then(() => {
+      basicLetter.value.tryCount++;
+      sr.start();
+    })
+    .catch((err) => {
+      err;
+      alert("실패");
+    });
+};
 
 const callApi = () => {
   api
@@ -59,6 +134,7 @@ const callApi = () => {
       router.replace("/");
     });
 };
+
 callApi();
 
 const next = () => {
