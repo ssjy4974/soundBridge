ae523f29938cbc27353174cb032969439c101f0b
unknown <gkh4302@naver.com>
2023-03-27 17:38:34 +0900

    S08P22A703-329 feat : 일상단어 등록 로직 추가

--- ai/models.py
+++ ai/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, BigInteger, String
+from sqlalchemy import Column, BigInteger, String, ForeignKey, Integer
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
@@ -16,3 +16,27 @@ class Record_State(Base):
   record_state_id = Column(BigInteger, primary_key=True, autoincrement=True)
   member_id = Column(BigInteger, nullable=False)
   sentence_id = Column(BigInteger, nullable=False)
+
+class Member(Base):
+  __tablename__ = 'member'
+  member_id = Column(BigInteger, primary_key=True, autoincrement=True)
+  voice_id = Column(BigInteger, nullable=True)
+  email = Column(String, nullable=False)
+  profile = Column(String, nullable=True)
+  age = Column(Integer, nullable=True)
+  gender = Column(String, nullable=True)
+  role = Column(String, nullable=True)
+  nickname = Column(String, nullable=False)
+  del_flag = Column(Integer, nullable=False)
+
+class Word_Member(Base):
+  __tablename__ = 'word_member'
+  word_member_id = Column(BigInteger, primary_key=True, autoincrement=True)
+  member_id = Column(BigInteger, ForeignKey("member.member_id"), nullable=False)
+  daily_word_id = Column(BigInteger, ForeignKey("daily_word.daily_word_id"), nullable=False)
+
+class Daily_Word(Base):
+  __tablename__ = 'daily_word'
+  daily_word_id = Column(BigInteger, primary_key=True, autoincrement=True)
+  word = Column(String, nullable=False)
+  guide_word = Column(String, nullable=False)
--- backend/src/main/java/com/soundbridge/domain/pronunciation/service/DailyWordService.java
+++ backend/src/main/java/com/soundbridge/domain/pronunciation/service/DailyWordService.java
@@ -1,8 +1,13 @@
 package com.soundbridge.domain.pronunciation.service;
 
+import com.soundbridge.domain.member.entity.Member;
+import com.soundbridge.domain.member.repository.MemberRepository;
 import com.soundbridge.domain.pronunciation.repository.DailyWordRepository;
 import com.soundbridge.domain.pronunciation.response.DailyWordRes;
+import com.soundbridge.global.error.ErrorCode;
+import com.soundbridge.global.error.exception.NotFoundException;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,14 +18,18 @@ import org.springframework.transaction.annotation.Transactional;
 public class DailyWordService {
 
     private final DailyWordRepository dailyWordRepository;
+    private final MemberRepository memberRepository;
 
     /**
-     * 일상 단어 전체 조회
+     * 일상 단어 상세 조회
      *
-     * @param memberId
+     * @param dailyWord
      * @return
      */
-//    public List<DailyWordRes> findAllDailyWords(Long memberId) {
-//        return dailyWordRepository.findAllByMemberId(memberId);
+//    public List<DailyWordRes> findDailyWord(String dailyWord, Long memberId) {
+//        final Member member = memberRepository.findById(memberId).orElseThrow(() ->
+//            new NotFoundException(ErrorCode.MEMBER_NOT_FOUND));
+//
+//        return dailyWordRepository.findDailyWordBydailyWord(dailyWord);
 //    }
 }
