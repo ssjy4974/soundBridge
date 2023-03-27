96b1c7baa5d0b8326e258de3f4e170deb991a46d
Ohsesol <tpthf1@gmail.com>
2023-03-27 23:51:21 +0900

    Design : 봉사자 마이페이지 디자인

--- frontend/src/components/mypage/RecordState.vue
+++ frontend/src/components/mypage/RecordState.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="text">현재 녹음 중인 문장</div>
+  <div class="container">
+    <div class="title1">현재 녹음 중인 문장</div>
 
-  <div>{{ sentence }}</div>
+    <div class="sentence">{{ sentence }}</div>
 
-  <div class="text">목소리 생성 진행도</div>
-  <div id="myProgress">
-    <div id="myBar">{{ per }}%</div>
-  </div>
+    <div class="title2">목소리 생성 진행도</div>
+    <div class="myProgress">
+      <div class="myBar"></div>
+      <div class="percent">{{ per }}%</div>
+    </div>
 
-  <div id="buttonSection">
-    <button id="recordBtn" v-if="per == 0" @click="startRecord">
-      기부 시작하기
-    </button>
-    <button id="recordBtn" v-else @click="toRecord">기부 이어하기</button>
+    <div class="buttonSection">
+      <button class="recordBtn" v-if="per == 0" @click="startRecord">
+        기부 시작하기
+      </button>
+      <button class="recordBtn" v-else @click="toRecord">기부 이어하기</button>
+    </div>
   </div>
 </template>
 
@@ -30,7 +33,7 @@ onBeforeMount(() => {
   api.get(`/api/records/1/`).then((res) => {
     sentence.value = res.data.content;
     per.value = ((res.data.sentenceId / 3922) * 100).toFixed(1);
-    let elem = document.getElementById("myBar");
+    let elem = document.querySelector(".myBar");
     var width = 1;
     var id = setInterval(frame, 10);
     function frame() {
@@ -41,7 +44,6 @@ onBeforeMount(() => {
         elem.style.width = width + "%";
       }
     }
-    elem.innerHTML = per.value + "%";
   });
 });
 
@@ -57,7 +59,12 @@ const startRecord = () => {
 </script>
 
 <style scoped>
-#recordBtn {
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.recordBtn {
   width: 280px;
   height: 32px;
   background-color: #2bacff;
@@ -66,26 +73,48 @@ const startRecord = () => {
   line-height: 0px;
 }
 
-#myProgress {
-  width: 280px;
+.myProgress {
+  width: 380px;
+  height: 20px;
+  margin-top: 10px;
   border-radius: 32px;
   background-color: #f1f1f1;
+  display: flex;
+  position: relative;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.12), 0 5px 5px rgba(0, 0, 0, 0.22);
 }
 
-#myBar {
+.myBar {
   width: 0%;
-  height: 30px;
+  height: 20px;
   border-radius: 32px;
   background-color: #8ad1ff;
-  text-align: center;
   color: #0b76bb;
 }
-
-#buttonSection {
-  margin-top: 20px;
+.percent {
+  position: absolute;
+  left: 165px;
 }
 
-.text {
+.buttonSection {
+  margin-top: 40px;
+}
+
+.title1 {
   font-weight: bold;
+  margin-top: 20px;
+  margin-left: -250px;
+}
+
+.title2 {
+  font-weight: bold;
+  margin-top: 50px;
+  margin-left: -256px;
+}
+
+.sentence {
+  color: #46464680;
+  margin-top: 10px;
+  width: 400px;
 }
 </style>
