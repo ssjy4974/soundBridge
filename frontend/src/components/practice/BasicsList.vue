620ca79cf344f314fb73e48d5e3b6de6bc3adbd2
JunYong Sun <ssjy4974@naver.com>
2023-03-27 17:49:04 +0900

    S08P22A703-422 S08P22A703-423 design : 리스트, 상세 페이지 CSS

--- frontend/src/components/practice/BasicsList.vue
+++ frontend/src/components/practice/BasicsList.vue
@@ -1,18 +1,13 @@
 <template>
-  <div>
+  <div class="main">
     <h3>발음 교정 기본 자음모음 메뉴 페이지</h3>
-    <ul v-for="(basicLetter, index) in basicLetterList" :key="index">
-      <li>{{ basicLetter.basicLetterId }}</li>
-      <span> 자음 명 : {{ basicLetter.letter }}</span>
-      <br />
-      <span> 시도횟수 : {{ basicLetter.tryCount }}</span>
-      <br />
-      <span> 성공횟수 : {{ basicLetter.successCount }}</span>
-      <br />
-      <router-link :to="`/practicebasicsdetail/${basicLetter.basicLetterId}`">
-        <img :src="`${IMAGE_PATH}/${basicLetter.letterImage}`" />
-      </router-link>
-    </ul>
+    <div class="container">
+      <div v-for="(basicLetter, index) in basicLetterList" :key="index">
+        <router-link :to="`/practicebasicsdetail/${basicLetter.basicLetterId}`">
+          <img :src="`${IMAGE_PATH}/${basicLetter.letterImage}`" />
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -39,8 +34,29 @@ callApi();
 </script>
 
 <style scoped>
+h3 {
+  margin-left: 20px;
+}
+.main {
+  height: 100%;
+  width: 95%;
+}
+.container {
+  display: flex;
+  border: 1px solid lightgray;
+  border-radius: 10px;
+  margin: 8px;
+  padding-top: 5px;
+  background: #ffffff;
+  height: 100%;
+  width: 100%;
+  flex-wrap: wrap;
+  justify-content: stretch;
+}
 img {
-  width: 100px;
-  height: 100px;
+  width: 75px;
+  height: 75px;
+  padding: 3px;
+  padding-left: 6.5px;
 }
 </style>
--- frontend/src/views/PracticeBasicsDetail.vue
+++ frontend/src/views/PracticeBasicsDetail.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <h1>발음 교정 기본 자음모음 개별 디테일 페이지</h1>
     <basics-detail />
   </div>
 </template>
