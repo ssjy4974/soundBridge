c8ce93f357c67fd9fe5651162e7c1af6b3429645
unknown <gkh4302@naver.com>
2023-03-28 18:09:00 +0900

    feat : 일상단어 등록 로직 추가중

--- frontend/src/api/dailyword.js
+++ frontend/src/api/dailyword.js
@@ -5,7 +5,7 @@ const api = apiInstance();
 async function getMyDailyWord(accessToken, success, fail) {
   console.log("get my dailyword", accessToken);
   await api
-    .get(`/api/daily-words`, {
+    .get(`/ai/daily-words`, {
       headers: {
         "access-token": accessToken,
       },
@@ -14,4 +14,22 @@ async function getMyDailyWord(accessToken, success, fail) {
     .catch(fail);
 }
 
-export { getMyDailyWord };
+async function addMyDailyWord(word, accessToken, success, fail) {
+  await api
+    .post(
+      `/api/my-sentence-logs`,
+      {
+        dailyWord: word,
+        memberId: 1,
+      },
+      {
+        headers: {
+          "access-token": accessToken,
+        },
+      }
+    )
+    .then(success)
+    .catch(fail);
+}
+
+export { getMyDailyWord, addMyDailyWord };
