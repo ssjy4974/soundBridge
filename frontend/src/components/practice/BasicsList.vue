75ee335ee88944f6f50d9c2b9f91243434a5965d
JunYong Sun <ssjy4974@naver.com>
2023-03-29 14:25:18 +0900

    refactor : basicLetter -> pinia 리팩토링

--- frontend/src/components/practice/BasicsList.vue
+++ frontend/src/components/practice/BasicsList.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="main">
-    <div class="container" v-if="basicLetterList">
-      <div v-for="(basicLetter, index) in basicLetterList" :key="index">
+    <div class="container" v-if="basicLetters">
+      <div v-for="(basicLetter, index) in basicLetters" :key="index">
         <router-link :to="`/practicebasicsdetail/${basicLetter.basicLetterId}`">
           <div class="wrap">
             <img :src="`${IMAGE_PATH}/${basicLetter.letterImage}`" />
@@ -14,25 +14,14 @@
 </template>
 
 <script setup>
-import { apiInstance } from "@/api/index";
-import { ref } from "vue";
+import { useBasicLetterStore } from "@/store/BasicLetter";
+import { storeToRefs } from "pinia";
 
-const api = apiInstance();
-const basicLetterList = ref();
+const store = useBasicLetterStore();
+const { basicLetters } = storeToRefs(store);
 const IMAGE_PATH = import.meta.env.VITE_IMAGE_PATH;
 
-const callApi = () => {
-  api
-    .get(`/api/basic-letters`)
-    .then((res) => {
-      basicLetterList.value = res.data;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-};
-
-callApi();
+store.getBasicLetters();
 </script>
 
 <style scoped>
---
+++ frontend/src/store/BasicLetter.js
@@ -0,0 +1,84 @@
+import { defineStore } from "pinia";
+import {
+  getBasicLetterDetails,
+  getBasicLetterList,
+  updateTryCount,
+  updateSuccessCount,
+} from "@/api/basicLetter";
+import { ref } from "vue";
+import router from "@/router/index";
+import Swal from "sweetalert2";
+
+export const useBasicLetterStore = defineStore("basicLetter", () => {
+  const basicLetters = ref();
+  const basicLetter = ref();
+
+  const getBasicLetters = async (accessToken) => {
+    await getBasicLetterList(accessToken, ({ data }) => {
+      basicLetters.value = data;
+    });
+  };
+
+  const getBasicLetter = async (accessToken, basicLetterId) => {
+    await getBasicLetterDetails(
+      accessToken,
+      basicLetterId,
+      ({ data }) => {
+        basicLetter.value = data;
+      },
+      () => {
+        Swal.fire(
+          "상세 조회 실패",
+          "해당 id 값은 존재 하지 않습니다.",
+          "error"
+        );
+        router.replace("/practicebasics");
+      }
+    );
+  };
+
+  const tryPractice = async (accessToken, basicLetterId) => {
+    await updateTryCount(
+      accessToken,
+      basicLetterId,
+      () => {
+        basicLetter.value.tryCount++;
+      },
+      () => {
+        Swal.fire("연습 시작 실패", "", "error");
+      }
+    );
+  };
+
+  const successPratice = async (accessToken, basicLetterId) => {
+    await updateSuccessCount(
+      accessToken,
+      basicLetterId,
+      () => {
+        basicLetter.value.successCount++;
+
+        Swal.fire(
+          "성공!",
+          "성공횟수 : " +
+            basicLetter.value.successCount +
+            "<br/>" +
+            "시도 횟수 : " +
+            basicLetter.value.tryCount,
+          "success"
+        );
+      },
+      () => {
+        alert("다시 한번 시도 해주세요");
+      }
+    );
+  };
+
+  return {
+    getBasicLetters,
+    getBasicLetter,
+    tryPractice,
+    successPratice,
+    basicLetters,
+    basicLetter,
+  };
+});
