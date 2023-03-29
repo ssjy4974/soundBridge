5d8756770f2a836d78ccbfae6fa31d26173b02ec
JunYong Sun <ssjy4974@naver.com>
2023-03-29 16:26:20 +0900

    feat : main page STT 기능 추가

--- frontend/src/components/pronounce/OthersSTT.vue
+++ frontend/src/components/pronounce/OthersSTT.vue
@@ -1,16 +1,56 @@
 <template>
   <div class="STT__container">
     <div>
-      <p>stt가 표시 되는 곳.</p>
-      <p>play 아이콘 클릭하면STT 실행</p>
+      <p v-text="transcript"></p>
+      <!-- <p>play 아이콘 클릭하면STT 실행</p> -->
     </div>
-    <div class="microphone__icon">
+    <div class="microphone__icon" @click="ToggleMic">
       <font-awesome-icon icon="fa-solid fa-play" />
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, onMounted } from "vue";
+
+const transcript = ref("");
+const isRecording = ref(false);
+
+const Recognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const sr = new Recognition();
+
+onMounted(() => {
+  sr.continuous = true;
+  sr.interimResults = true;
+  // 언어 설정, 설정하지 않으면 디폴트로 HTML 의 lang 속성값을 받음
+  sr.lang = "ko-KR";
+  sr.maxAlternatives = 10000;
+  sr.onstart = () => {
+    console.log("발화 시작");
+    isRecording.value = true;
+  };
+  sr.onend = () => {
+    console.log("발화 종료");
+    isRecording.value = false;
+    transcript.value = "";
+  };
+  sr.onresult = (evt) => {
+    const t = Array.from(evt.results)
+      .map((result) => result[0])
+      .map((result) => result.transcript)
+      .join("");
+    transcript.value = t;
+  };
+});
+
+const ToggleMic = () => {
+  if (transcript.value) {
+    sr.stop();
+  } else {
+    sr.start();
+  }
+};
+</script>
 
 <style scoped>
 .STT__container {
