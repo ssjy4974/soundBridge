099adf18ca9ff0acb9093d766c9c5e3590681d96
안승태 <sng_a@naver.com>
2023-03-30 09:43:27 +0900

    style : login page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,28 +7,28 @@ import "@fortawesome/fontawesome-free/css/all.css";
 import FooterDisabled from "./components/FooterDisabled.vue";
 import Header from "./components/Header.vue";
 import FooterVolunteer from "./components/FooterVolunteer.vue";
+
+import { useMember } from "@/store/Member";
+const memberStore = useMember();
+const role = memberStore.role;
 </script>
 
 <template>
   <div>
-    <Header v-if="userRole != null" />
+    <Header v-if="role != null" />
     <div id="wrapper">
       <router-view
         v-bind:class="{
-          router: userRole === 'APPLICANT' || userRole === 'HELPER',
+          router: role === 'APPLICANT' || role === 'HELPER',
         }"
       />
     </div>
     <!-- 로그인이 되어있지 않을때 display : none, 장애인 유저 로그인 상태면 장애인용 푸터, 봉사자일 때 봉사자 푸터 -->
 
-    <FooterDisabled v-if="userRole === 'APPLICANT'" />
-    <FooterVolunteer v-if="userRole === 'HELPER'" />
+    <FooterDisabled v-if="role === 'APPLICANT'" />
+    <FooterVolunteer v-if="role === 'HELPER'" />
   </div>
 </template>
-
-<script>
-const userRole = "APPLICANT";
-</script>
 
 <style scoped>
 #wrapper {
