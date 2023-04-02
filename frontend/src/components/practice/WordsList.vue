f8ace2c08ec59a3e9bf14c98e515c141cac4ead3
leo9876 <leo9876@naver.com>
2023-04-02 10:06:51 +0900

    feat : 일상단어 CSS

--- frontend/src/components/practice/WordsList.vue
+++ frontend/src/components/practice/WordsList.vue
@@ -3,14 +3,68 @@
     <div class="parent">
       <div
         class="child"
-        @click=""
         v-for="(item, index) in MyDailyWord.mydailyword"
+        :key="index"
       >
         <router-link :to="`/wordsdetail/${index}`">
           {{ item.word }}
         </router-link>
       </div>
     </div>
+    <div>
+      <button @click="addWordModal">연습할 단어 추가하는 버튼</button>
+      <AddWordModal v-if="isWordModal" @closemodal="addWordModal" />
+    </div>
+    <!--  -->
+    <!-- <div class="parent">
+      <div class="child">
+        <div class="link__container">
+          <router-link :to="`/wordsdetail/${index}`">돈까스 </router-link>
+        </div>
+      </div>
+      <div class="child">
+        <router-link :to="`/wordsdetail/${index}`"> 돈까스 </router-link>
+      </div>
+      <div class="child">
+        <router-link :to="`/wordsdetail/${index}`"> 돈까스 </router-link>
+      </div>
+      <div class="child">
+        <router-link :to="`/wordsdetail/${index}`"> 돈까스 </router-link>
+      </div>
+      <div class="child">
+        <router-link :to="`/wordsdetail/${index}`"> 돈까스 </router-link>
+      </div>
+      <div class="child">
+        <router-link :to="`/wordsdetail/${index}`"> 돈까스 </router-link>
+      </div>
+      <div class="child">
+        <router-link :to="`/wordsdetail/${index}`"> 돈까스 </router-link>
+      </div>
+      <div class="child">
+        <router-link :to="`/wordsdetail/${index}`"> 돈까스 </router-link>
+      </div>
+      <div class="child">
+        <router-link :to="`/wordsdetail/${index}`"> 돈까스 </router-link>
+      </div>
+      <div class="child">
+        <router-link :to="`/wordsdetail/${index}`"> 돈까스 </router-link>
+      </div>
+      <div class="child">
+        <router-link :to="`/wordsdetail/${index}`"> 돈까스 </router-link>
+      </div>
+      <div class="child">
+        <router-link :to="`/wordsdetail/${index}`"> 돈까스 </router-link>
+      </div>
+      <div class="child">
+        <router-link :to="`/wordsdetail/${index}`"> 돈까스 </router-link>
+      </div>
+      <div class="child">
+        <router-link :to="`/wordsdetail/${index}`"> 돈까스 </router-link>
+      </div>
+      <div class="child">
+        <router-link :to="`/wordsdetail/${index}`"> 돈까스 </router-link>
+      </div>
+    </div> -->
     <div>
       <button @click="addWordModal">연습할 단어 추가하는 버튼</button>
       <AddWordModal v-if="isWordModal" @closemodal="addWordModal" />
@@ -33,27 +87,43 @@ const callAPI = () => {
 callAPI();
 
 const addWordModal = () => {
-  console.log("addWord form Wordmodal", isWordModal.value);
+  console.log("?????????", MyDailyWord.mydailyword);
+  // console.log("addWord form Wordmodal", isWordModal.value);
 
   isWordModal.value = !isWordModal.value;
-  console.log("isWordModal value", isWordModal.value);
+  // console.log("isWordModal value", isWordModal.value);
 };
 </script>
 
 <style lang="scss" scoped>
+.container {
+  // background-color: var(--maincolor2);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 .parent {
-  width: 90%;
-  border: 1px solid red;
-  margin: 0 auto;
+  margin-inline: 5vw;
+  // border: 1px solid var(--maincolor3);
+  // padding-inline: 5%;
+  padding-block: 10%;
   display: flex;
   flex-wrap: wrap;
   justify-content: space-evenly;
 }
 
 .child {
-  background-color: aqua;
-  width: 40%;
-  height: 70px;
-  margin: 20px 0;
+  background-color: var(--maincolor2);
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 8vh;
+  width: 40vw;
+  color: white;
+  margin-block: 3vh;
+}
+.child a {
+  // color: var(--black);
 }
 </style>
