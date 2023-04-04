4e1e69735475bfc0ebc994902e818dc5fadb46c9
오세솔 <tpthf1@gmail.com>
2023-04-04 09:21:37 +0900

    refactor : member 반환 정보 voiceId 추가

--- backend/src/main/java/com/soundbridge/domain/member/response/MemberInfoRes.java
+++ backend/src/main/java/com/soundbridge/domain/member/response/MemberInfoRes.java
@@ -1,7 +1,6 @@
 package com.soundbridge.domain.member.response;
 
 import com.soundbridge.domain.member.entity.Role;
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,26 +14,29 @@ public class MemberInfoRes {
     private String nickname;
     private String profile;
     private Role role;
+    private Long voiceId;
 
     @Builder
     public MemberInfoRes(Long memberId, String email, String nickname, String profile,
-        Role role) {
+            Role role, Long voiceId) {
         this.memberId = memberId;
         this.email = email;
         this.nickname = nickname;
         this.profile = profile;
         this.role = role;
+        this.voiceId = voiceId;
     }
 
     public static MemberInfoRes of(Long memberId, String email, String nickname, String profile,
-        Role role) {
+            Role role, Long voiceId) {
         return MemberInfoRes.builder()
-            .memberId(memberId)
-            .email(email)
-            .nickname(nickname)
-            .profile(profile)
-            .role(role)
-            .build();
+                .memberId(memberId)
+                .email(email)
+                .nickname(nickname)
+                .profile(profile)
+                .role(role)
+                .voiceId(voiceId)
+                .build();
 
     }
 }
