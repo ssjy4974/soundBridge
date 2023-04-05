fb9ded93eade00c1ff2833f5ba64f22521663a24
leo9876 <leo9876@naver.com>
2023-04-05 19:41:27 +0900

    style: 역할 선택 페이지 CSS

--- frontend/src/components/RoleExplain.vue
+++ frontend/src/components/RoleExplain.vue
@@ -1,14 +1,26 @@
 <template>
   <div class="role-explain">
     <h1>역할 설명</h1>
-    <p id="chung">
-      청인 : 목소리를 기부하고, 농인들의 발음교정을 온라인 미팅으로 도와주는
-      역할입니다.
-    </p>
-    <p id="nong">
-      농인 : TTS와, STT를 활용하여 목소리를 사용하고, 상대방의 말을 문장으로
-      받아 볼 수 있습니다. 또한, 발음교정을 도움받을 수 있습니다.
-    </p>
+    <div class="description__container">
+      <div class="volunteer__container">
+        <div class="volunteer__name"><p>청인 :</p></div>
+        <div class="volunteer__explain">
+          <p class="volunteer__explain">
+            목소리를 기부하고, 농인들의 발음교정을 온라인 미팅으로 도와주는
+            역할입니다.
+          </p>
+        </div>
+      </div>
+      <div class="deaf__container">
+        <div class="deaf__name"><p>농인 :</p></div>
+        <div class="deaf__explain">
+          <p>
+            TTS와, STT를 활용하여 목소리를 사용하고, 상대방의 말을 문장으로 받아
+            볼 수 있습니다. 또한, 발음교정을 도움받을 수 있습니다.
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -27,5 +39,24 @@ h1 {
 }
 .role-explain {
   padding: 4%;
+}
+.volunteer__container {
+  padding-inline: 1%;
+  line-height: 1.7rem;
+  display: flex;
+}
+.volunteer__name {
+  white-space: nowrap;
+  width: 20%;
+}
+.deaf__container {
+  line-height: 1.7rem;
+  display: flex;
+}
+.deaf__name {
+  padding-inline: 1%;
+  line-height: 1.7rem;
+  white-space: nowrap;
+  width: 20%;
 }
 </style>
