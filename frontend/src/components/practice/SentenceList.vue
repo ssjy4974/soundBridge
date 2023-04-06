2baa2f73ca9b73eff1a7df43d814c34880e33856
unknown <gkh4302@naver.com>
2023-04-06 17:40:31 +0900

    fix : 성공률 정수형 수정

--- frontend/src/components/practice/SentenceList.vue
+++ frontend/src/components/practice/SentenceList.vue
@@ -55,7 +55,7 @@ onUpdated(() => {
     if (element.tryCount == 0) {
       percent = 0;
     } else {
-      percent = (element.successCount / element.tryCount) * 100;
+      percent = Number((element.successCount / element.tryCount) * 100);
     }
     per.value.push(percent.toFixed(1));
     let elem = document.querySelector(`#mybar${index}`);
