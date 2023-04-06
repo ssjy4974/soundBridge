89588cde548fc275f7e1bc2022e6afc195175c0f
unknown <gkh4302@naver.com>
2023-04-06 17:53:54 +0900

    fix : 마지막 수정

--- frontend/src/components/practice/WordsList.vue
+++ frontend/src/components/practice/WordsList.vue
@@ -57,7 +57,7 @@ onUpdated(() => {
     if (element.tryCount == 0) {
       percent = 0;
     } else {
-      percent = ((element.successCount / element.tryCount) * 100).toFixed(1);
+      percent = Math.floor(element.successCount / element.tryCount) * 100;
     }
     per.value.push(percent.toFixed(1));
     let elem = document.querySelector(`#mybar${element.wordMemberId}`);
